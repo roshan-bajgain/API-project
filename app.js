878f33381645ca81793846ce757c4301f475e268
Roshan Bajgain <roshan.bazgain@gmail.com>
2021-07-05 10:19:44 +0545

    add click event

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 const colors = ["green", "red", "rgba"(133,122,200), "#f15025"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
+btn.addEventListener("click",function(){
+    const randomNumberm = 2;
+    document.body.style.backgroundColor = color[randomNumber];
+    color.textContent =  colors[randomNumber];
+});
